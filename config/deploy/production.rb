70c95150d6f8deb44919f960873e49c4d0f0456a
“keita0716” <keita1192.r@gmail.com>
2020-01-02 03:37:36 +0900

    Revert "自動デプロイ"
    This reverts commit 0dd6f44666336cd8cb4c69109bb4944c04994469.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.69.233.17', user: 'ec2-user', roles: %w{app db web}
+server '54.238.165.86', user: 'ec2-user', roles: %w{app db web}
