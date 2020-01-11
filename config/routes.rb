9b6abf45ce2b1c7748ea35bc9bd135826e110ddd
“keita0716” <keita1192.r@gmail.com>
2020-01-11 16:27:39 +0900

    resourcesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-  resources :mypage, only: [:index, :edit]
-  get 'mypage/identification' => 'identification#index'
-end
+  resources :mypage, only: [:index, :edit, :identification] do
+    resources :identification, only: [:index]
+  end
+  end
