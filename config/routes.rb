4738aa8dcb18b5bcce95a0dc1004066d35bf2887
“keita0716” <keita1192.r@gmail.com>
2020-01-11 16:36:10 +0900

    不要な記述の削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-  resources :mypage, only: [:index, :edit, :identification] do
+  resources :mypage, only: [:index, :edit] do
     resources :identification, only: [:index]
   end
 end
