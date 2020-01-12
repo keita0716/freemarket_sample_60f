725eb68b1d952af6a98c37f0fd2abbe6bce06dcb
takanotty <i.kunot.shukatsu@gmail.com>
2020-01-12 19:09:11 +0900

    set_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-  resources :mypage, only: [:index, :edit] do
+  resources :items
+  resources :mypage, only: [:index, :create, :edit, :update] do
     resources :identification, only: [:index]
   end
 end
