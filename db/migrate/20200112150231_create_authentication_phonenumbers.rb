909f1835d590216f7b882dab951656641ef51a9d
yuhakare <iheuyo927@yahoo.co.jp>
2020-01-14 00:02:59 +0900

    ユーザー登録機能実装

--- db/migrate/20200112150231_create_authentication_phonenumbers.rb
+++ db/migrate/20200112150231_create_authentication_phonenumbers.rb
@@ -3,7 +3,6 @@ class CreateAuthenticationPhonenumbers < ActiveRecord::Migration[5.2]
     create_table :authentication_phonenumbers do |t|
       t.integer :authentication_phonenumber,                       null: false
       t.references :user, null: false, index: true, foreign_key: true
-
       t.timestamps
     end
   end
