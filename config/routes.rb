b2c86a20e6a3ed12a065dd2d8bf712876c139a7f
takanotty <i.kunot.shukatsu@gmail.com>
2020-02-16 12:21:04 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
-  resources :items
+  resources :items, only: [:new, :create]
   resources :mypage, only: [:index, :create, :edit, :update] do
     resources :identification, only: [:index]
   end
