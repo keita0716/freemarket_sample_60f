5f575a74fb7435089c663d78f721f8c01ebdce56
takanotty <i.kunot.shukatsu@gmail.com>
2020-02-19 00:10:34 +0900

    アイテム一覧実装（画像以外）

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,6 @@
 class TopsController < ApplicationController
   def index
+    @items = Item.where(trade_status: "出品中").order(id: "DESC").first(10)
+    #@image = @items.images.first .includes(:image)
   end
 end
