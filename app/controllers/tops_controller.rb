f9b761c2dd7073dadd0614fa31d23bc4212a7785
takanotty <i.kunot.shukatsu@gmail.com>
2020-02-20 22:26:50 +0900

    後程実装分を作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,18 @@
 class TopsController < ApplicationController
+  
+  #以下、コメントアウト部分は後程実装予定分
+  #before_action :set_item
+
   def index
     @items = Item.where(trade_status: "出品中").order(id: "DESC").first(10)
-    #@image = @items.images.first .includes(:image)
+    #@categorys = @items.joins(:category).group("categories.name").order(count_name: "DESC").first(4)
+    #@categories_item = @categories.order(id: "DESC").first(10)
+    #@brands = @items.joins(:brand).group("brands.name").order(count_name: "DESC").first(4)
+    #@brands_item = @brands.order(id: "DESC").first(10)
   end
+
+  def set_item
+    #@items = Item.where(trade_status: "出品中")
+  end
+
 end
