b99d2dab3ed661a2c1e639122013e27b3e3ee104
takanotty <i.kunot.shukatsu@gmail.com>
2020-02-23 00:57:12 +0900

    ブランド別一覧完成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,8 +1,8 @@
 class TopsController < ApplicationController
 
   def index
-    @category = Item.group(:category_id).order('count(category_id) DESC').includes(:category)
-    #@category_items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id: "DESC").includes(:images)
+    @category = Item.group(:category_id).order('count(category_id) DESC')
+    @brand = Item.group(:brand_id).order('count(brand_id) DESC')
     @items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id: "DESC").includes(:images)
     #@categories = @item.joins(:category).group("categories.name").order(count_name: "DESC").first(4)
     #@categories_item = @categories.order(id: "DESC").first(10)
