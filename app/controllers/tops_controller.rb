da4e1dee7db3d9fc07af9c36e9736ab32c4f5772
takanotty <i.kunot.shukatsu@gmail.com>
2020-02-22 19:21:05 +0900

    商品一覧（カテゴリー・ブランド不分別）完成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,18 +1,13 @@
 class TopsController < ApplicationController
   
   #以下、コメントアウト部分は後程実装予定分
-  #before_action :set_item
 
   def index
-    @items = Item.where(trade_status: "出品中").order(id: "DESC").first(10)
+    @items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id: "DESC").includes(:images)
     #@categories = @item.joins(:category).group("categories.name").order(count_name: "DESC").first(4)
     #@categories_item = @categories.order(id: "DESC").first(10)
     #@brands = @items.joins(:brand).group("brands.name").order(count_name: "DESC").first(4)
     #@brands_item = @brands.order(id: "DESC").first(10)
-  end
-
-  def set_item
-    #@items = Item.where(trade_status: "出品中")
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_074312) do
+ActiveRecord::Schema.define(version: 2020_02_19_114030) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_family_name", null: false
