d215dae5accb05903f8ecba6f9770e0d6ea5b376
“keita0716” <keita1192.r@gmail.com>
2020-02-22 10:05:14 +0900

    categoryのデータ投入seedsの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/category.csv') do |row|
+  Category.create(:category_left_id => row[1], 
+                  :category_left_name => row[2],
+                  :category_center_id => row[3],
+                  :category_center_name => row[4],
+                  :category_right_id => row[5],
+                  :category_right_name => row[6],
+                  :item_id => row[7]
+                )
+end
