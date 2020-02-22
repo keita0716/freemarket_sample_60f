b1e5ae17788447861a284d57d46507edd4d2ab59
takanotty <i.kunot.shukatsu@gmail.com>
2020-02-22 21:28:01 +0900

    人気のカテゴリー作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -3,11 +3,15 @@ class TopsController < ApplicationController
   #以下、コメントアウト部分は後程実装予定分
 
   def index
+    @category = Item.group(:category_id).order('count(category_id) DESC').includes(:category)
+    #@category_items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id: "DESC").includes(:images)
     @items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id: "DESC").includes(:images)
     #@categories = @item.joins(:category).group("categories.name").order(count_name: "DESC").first(4)
     #@categories_item = @categories.order(id: "DESC").first(10)
     #@brands = @items.joins(:brand).group("brands.name").order(count_name: "DESC").first(4)
     #@brands_item = @brands.order(id: "DESC").first(10)
   end
+
+    
 
 end
