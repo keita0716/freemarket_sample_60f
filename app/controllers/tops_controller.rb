29362ee08bd373554a212034ba45f2bf17d03602
“keita0716” <keita1192.r@gmail.com>
2020-02-24 23:12:09 +0900

    brand_idが空の時の対応

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,10 +2,8 @@ class TopsController < ApplicationController
 
   def index
     @category = Item.joins(:category).group(:category_left_id).order('count(category_left_id) DESC')
-    @brand = Item.group(:brand_id).order('count(brand_id) DESC')
+    @brand = Item.group(:brand_id).order('count(brand_id) DESC').where("brand_id is not null")
     @items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id:"DESC").includes(:images)
   end
-
-    
-
+  
 end
