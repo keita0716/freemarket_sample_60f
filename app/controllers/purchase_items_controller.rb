e26c85e8105cd6f4560373ce657f42f6ae369724
yuhakare <iheuyo927@yahoo.co.jp>
2020-02-29 19:13:40 +0900

    set_card設定

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -1,41 +1,30 @@
 class PurchaseItemsController < ApplicationController
   before_action :set_infomation
+  before_action :set_card
   def show
-    card = CreditCard.find_by(user_id: current_user.id)
-    if card.blank?
-      #flash[:alert] = '購入にはクレジットカード登録が必要です'
-      redirect_to controller: 'credit_cards', action: 'new_credit_card'
-    else
-      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @customer_card = customer.cards.retrieve(card.card_id)    
-    end
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @customer_card = customer.cards.retrieve(@card.card_id)    
   end
 
   def purchase_complete
-    card = CreditCard.find_by(user_id: current_user.id)
-    if card.blank?
-      redirect_to controller: 'credit_cards', action: 'new_credit_card'
-      #flash[:alert] = '購入にはクレジットカード登録が必要です'
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @customer_card = customer.cards.retrieve(@card.card_id)  
+    # 購入した際の情報を元に引っ張ってくる
+    # テーブル紐付けてるのでログインユーザーのクレジットカードを引っ張ってくる
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
+    # キーをセットする(環境変数においても良い)
+    Payjp::Charge.create(
+    amount: @item.price, #支払金額
+    customer: @card.customer_id, #顧客ID
+    currency: 'jpy', #日本円
+    )
+    # 商品の金額をamountへ、cardの顧客idをcustomerへ、currencyをjpyへ入れる
+    if @item.update(buyer_id: current_user.id)
+      #flash[:notice] = '購入しました。'
     else
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @customer_card = customer.cards.retrieve(card.card_id)  
-      # 購入した際の情報を元に引っ張ってくる
-      # テーブル紐付けてるのでログインユーザーのクレジットカードを引っ張ってくる
-      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-      # キーをセットする(環境変数においても良い)
-      Payjp::Charge.create(
-      amount: @item.price, #支払金額
-      customer: card.customer_id, #顧客ID
-      currency: 'jpy', #日本円
-      )
-      # 商品の金額をamountへ、cardの顧客idをcustomerへ、currencyをjpyへ入れる
-      if @item.update(buyer_id: current_user.id)
-        #flash[:notice] = '購入しました。'
-      else
-        #flash[:alert] = '購入に失敗しました。'
-        redirect_to action: 'show'
-      end
+      #flash[:alert] = '購入に失敗しました。'
+      redirect_to action: 'show'
     end
   end
 
@@ -49,4 +38,11 @@ class PurchaseItemsController < ApplicationController
     @addresses = Address.find_by(user_id: current_user.id)
   end
 
+  def set_card
+    @card = CreditCard.find_by(user_id: current_user.id)
+    if @card.blank?
+      #flash[:alert] = '購入にはクレジットカード登録が必要です'
+      redirect_to controller: 'credit_cards', action: 'new_credit_card'
+    end
+  end
 end
