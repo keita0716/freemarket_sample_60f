40f52e308cf57e14892a5e72826340c3f589e70c
“keita0716” <keita1192.r@gmail.com>
2020-02-29 13:05:35 +0900

    エラーハンドリング処理の追加

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,10 +1,16 @@
 class Api::ItemsController < ApplicationController
 
   def image_destroy
-    image = Image.find(params[:img_id])
-    if image.item.seller_id == current_user.id
-      image.destroy
+    begin
+      image = Image.find(params[:img_id])
+      if image.item.seller_id == current_user.id
+        image.destroy
+      end
+    rescue
+      puts "削除に失敗しました"
     end
+
+
   end
 
 end
