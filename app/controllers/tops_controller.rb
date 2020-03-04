2ec8794f3e724c7048085fb97cd9268e43b2df5d
“keita0716” <keita1192.r@gmail.com>
2020-03-04 18:46:57 +0900

    topページ微調整

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,8 +1,8 @@
 class TopsController < ApplicationController
 
   def index
-    @category = Item.joins(:category).group(:category_left_id).order('count(category_left_id) DESC')
-    @brand = Item.group(:brand_id).order('count(brand_id) DESC').where("brand_id is not null")
+    @category = Item.joins(:category).group(:category_left_id).where("buyer_id IS NULL").where("auction_id IS NULL").order('count(category_left_id) DESC')
+    @brand = Item.group(:brand_id).order('count(brand_id) DESC').where("brand_id is not null").where("buyer_id IS NULL").where("auction_id IS NULL")
     @items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id:"DESC").includes(:images)
   end
   
