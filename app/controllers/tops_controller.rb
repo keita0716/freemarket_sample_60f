1121ae123a782a25e2f1cdf972fcedd7706711c8
takanotty <i.kunot.shukatsu@gmail.com>
2020-03-05 01:09:57 +0900

    いろいろ修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,9 +1,12 @@
 class TopsController < ApplicationController
 
   def index
-    @category = Item.joins(:category).group(:category_left_id).where("buyer_id IS NULL").where("auction_id IS NULL").order('count(category_left_id) DESC')
-    @brand = Item.group(:brand_id).order('count(brand_id) DESC').where("brand_id is not null").where("buyer_id IS NULL").where("auction_id IS NULL")
-    @items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id:"DESC").includes(:images)
+    @category = Item.joins(:category).group(:category_left_id).order('count(category_left_id) DESC')
+    @brand = Item.group(:brand_id).order('count(brand_id) DESC').where("brand_id is not null")
+    @items = Item.order(id:"DESC").includes(:images)
+    #@category = Item.joins(:category).group(:category_left_id).where("buyer_id IS NULL").where("auction_id IS NULL").order('count(category_left_id) DESC')
+    #@brand = Item.group(:brand_id).order('count(brand_id) DESC').where("brand_id is not null").where("buyer_id IS NULL").where("auction_id IS NULL")
+    #@items = Item.where("buyer_id IS NULL").where("auction_id IS NULL").order(id:"DESC").includes(:images)
   end
   
 end
