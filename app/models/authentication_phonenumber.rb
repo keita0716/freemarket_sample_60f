2b47b471f7702a84b9deb194ed9dfb84cabc4bff
yuhakare <iheuyo927@yahoo.co.jp>
2020-03-05 21:04:22 +0900

    validation&price_max_min

--- app/models/authentication_phonenumber.rb
+++ app/models/authentication_phonenumber.rb
@@ -1,4 +1,4 @@
 class AuthenticationPhonenumber < ApplicationRecord
   belongs_to :user, optional: true
-  validates :authentication_phonenumber,presence: true, length: { maximum: 11 }
+  validates :authentication_phonenumber,presence: true, length: { maximum: 11 }, numericality: { only_integer: true }
 end
